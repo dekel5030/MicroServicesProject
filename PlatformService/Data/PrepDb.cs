2c5ea3924e3674f88f0b84655cf3375a12c75899
Dekel5030 <dekel5030@gmail.com>
2025-07-21 23:30:38 +0300

    Added mssql server

--- PlatformService/Data/PrepDb.cs
+++ PlatformService/Data/PrepDb.cs
@@ -1,42 +1,56 @@
+using Microsoft.EntityFrameworkCore;
 using PlatformService.Models;
 
 namespace PlatformService.Data
 {
     public static class PrepDb
     {
-        public static void PrepPopulation(IApplicationBuilder app)
+        public static void PrepPopulation(IApplicationBuilder app, bool IsProduction)
         {
             using (var serviceScope = app.ApplicationServices.CreateScope())
             {
-                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>(), IsProduction);
             }
         }
 
-        private static void SeedData(AppDbContext? context)
+        private static void SeedData(AppDbContext? context, bool IsProduction)
         {
             if (context == null)
             {
                 throw new ArgumentNullException(nameof(context), "AppDbContext is not registered in the DI container.");
             }
+
+            if (IsProduction)
+            {
+                Console.WriteLine("--> Attempting to apply migrations...");
+                try
+                {
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"--> Could not run migration {ex.Message}");
+                }
+            }
             
             if (!context.Platforms.Any())
-                {
-                    Console.WriteLine("--> Seeding data...");
-                    context.Platforms.AddRange(
-                        new Platform { Name = "Dot Net", Publisher = "Microsoft", Cost = "Free" },
-                        new Platform { Name = "Node.js", Publisher = "OpenJS Foundation", Cost = "Free" },
-                        new Platform { Name = "Java", Publisher = "Oracle", Cost = "Free" },
-                        new Platform { Name = "Python", Publisher = "Python Software Foundation", Cost = "Free" },
-                        new Platform { Name = "Ruby on Rails", Publisher = "Rails Core Team", Cost = "Free" },
-                        new Platform { Name = "Spring Boot", Publisher = "VMware", Cost = "Free" }
-                    );
+            {
+                Console.WriteLine("--> Seeding data...");
+                context.Platforms.AddRange(
+                    new Platform { Name = "Dot Net", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform { Name = "Node.js", Publisher = "OpenJS Foundation", Cost = "Free" },
+                    new Platform { Name = "Java", Publisher = "Oracle", Cost = "Free" },
+                    new Platform { Name = "Python", Publisher = "Python Software Foundation", Cost = "Free" },
+                    new Platform { Name = "Ruby on Rails", Publisher = "Rails Core Team", Cost = "Free" },
+                    new Platform { Name = "Spring Boot", Publisher = "VMware", Cost = "Free" }
+                );
 
-                    context.SaveChanges();
-                }
-                else
-                {
-                    Console.WriteLine("--> We already have data");
-                }
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("--> We already have data");
+            }
         }
     }
 }
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -5,10 +5,22 @@ using PlatformService.SyncDataServices.Http;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<AppDbContext>(option =>
+if (builder.Environment.IsProduction())
 {
-    option.UseInMemoryDatabase("InMem");
-});
+    Console.WriteLine("--> Using SqlServer Db");
+    builder.Services.AddDbContext<AppDbContext>(option =>
+    {
+        option.UseSqlServer(builder.Configuration.GetConnectionString("platformsConn"));
+    });
+}
+else
+{
+    Console.WriteLine("--> Using InMem Db");
+    builder.Services.AddDbContext<AppDbContext>(option =>
+    {
+        option.UseInMemoryDatabase("InMem");
+    });
+}
 
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddControllers();
@@ -19,7 +31,7 @@ builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 Console.WriteLine($"--> CommandService Endpoint {builder.Configuration["CommandService"]}");
 
 var app = builder.Build();
-PrepDb.PrepPopulation(app);
+PrepDb.PrepPopulation(app, builder.Environment.IsProduction());
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -27,7 +39,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
